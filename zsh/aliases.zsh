d3ea1863cb641186f716cd9b0c669158c91b818b
Sam Couch <samcouch91@gmail.com>
2015-04-30 01:14:05 -0400

    update alliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -2,5 +2,9 @@ alias reload!='. ~/.zshrc'
 alias h='cd ~/'
 
 alias c='clear'
+alias pk='pkill -9'
 
 alias expose='ngrok -authtoken $NGROKTOKEN --subdomain=couch'
+
+alias mkdir='mkdir -pv'
+alias finder='open ./'
